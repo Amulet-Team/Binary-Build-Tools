28f8ad2226529c52f20b08524a8f5c5ce0623d32
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-19 11:22:58 +0100

    Add Amulet-Anvil files

---
+++ generated/Amulet-Anvil/tests/test_amulet_anvil/__init__.py
@@ -0,0 +1,25 @@
+if __name__ != "test_amulet_anvil":
+    raise RuntimeError(
+        f"Module name is incorrect. Expected: 'test_amulet_anvil' got '{__name__}'"
+    )
+
+
+import faulthandler
+
+faulthandler.enable()
+
+
+def _init() -> None:
+    import sys
+
+    # Import dependencies
+    import amulet.anvil
+
+    # This needs to be an absolute path otherwise it may get called twice
+    # on different module objects and crash when the interpreter shuts down.
+    from test_amulet_anvil._test_amulet_anvil import init
+
+    init(sys.modules[__name__])
+
+
+_init()
