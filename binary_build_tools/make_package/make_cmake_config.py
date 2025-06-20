7dcf2594554cc29f3cbffa6b65c16c5f89163533
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-20 09:48:18 +0100

    Standardise cmake config files

--- binary_build_tools/make_package/make_cmake_config.py
+++ binary_build_tools/make_package/make_cmake_config.py
@@ -1,13 +1,18 @@
 import os
 
-from binary_build_tools.data import LibraryData
+from binary_build_tools.data import LibraryData, libraries
 
 
 def write(package_path: str, library_data: LibraryData) -> None:
-    with open(os.path.join(package_path, f"{library_data.lib_name}Config.cmake"), "w", encoding="utf-8") as f:
-        f.write(f"""if (NOT TARGET {library_data.lib_name})
+    with open(
+        os.path.join(package_path, f"{library_data.lib_name}Config.cmake"),
+        "w",
+        encoding="utf-8",
+    ) as f:
+        f.write(
+            f"""if (NOT TARGET {library_data.lib_name})
     message(STATUS "Finding {library_data.lib_name}")
-
+{f"\n    {"\n    ".join(f"find_package({libraries[lib].cmake_package} CONFIG REQUIRED)" for lib in library_data.public_dependencies)}\n" if library_data.public_dependencies else ""}
     set({library_data.lib_name}_INCLUDE_DIR "${{CMAKE_CURRENT_LIST_DIR}}/../..")
     find_library({library_data.lib_name}_LIBRARY NAMES {library_data.lib_name} PATHS "${{CMAKE_CURRENT_LIST_DIR}}")
     message(STATUS "{library_data.lib_name}_LIBRARY: ${{{library_data.lib_name}_LIBRARY}}")
@@ -17,8 +22,11 @@ def write(package_path: str, library_data: LibraryData) -> None:
         IMPORTED_IMPLIB "${{{library_data.lib_name}_LIBRARY}}"
     )
 
-    add_library({library_data.lib_name} INTERFACE)
+    add_library({library_data.lib_name} INTERFACE){
+    "".join(f"\n    target_link_libraries({library_data.lib_name} INTERFACE {libraries[lib].cmake_lib_name})" for lib in library_data.public_dependencies) if library_data.public_dependencies else ""
+    }
     target_link_libraries({library_data.lib_name} INTERFACE {library_data.lib_name}_bin)
     target_include_directories({library_data.lib_name} INTERFACE ${{{library_data.lib_name}_INCLUDE_DIR}})
 endif()
-""")
+"""
+        )
