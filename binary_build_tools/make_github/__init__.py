77ad884ffd442036e2a9a4fcf97f1351be9e39d5
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-23 13:52:18 +0100

    Standardise the install action

--- binary_build_tools/make_github/__init__.py
+++ binary_build_tools/make_github/__init__.py
@@ -3,6 +3,7 @@ import os
 from binary_build_tools.data import LibraryData
 
 from . import (
+    make_install,
     make_install_dependencies,
     make_stylecheck,
 )
@@ -11,6 +12,7 @@ from . import (
 def write(github_path: str, library_data: LibraryData) -> None:
     actions_path = os.path.join(github_path, "actions")
     os.makedirs(actions_path, exist_ok=True)
+    make_install.write(actions_path, library_data)
     make_install_dependencies.write(actions_path, library_data)
     workflows_path = os.path.join(github_path, "workflows")
     os.makedirs(workflows_path, exist_ok=True)
---
+++ binary_build_tools/make_github/make_install.py
@@ -0,0 +1,155 @@
+import os
+
+from binary_build_tools.data import LibraryData, libraries, library_order, LibraryType
+
+
+def write(actions_path: str, library_data: LibraryData) -> None:
+    action_dir = os.path.join(actions_path, "install")
+    os.makedirs(action_dir, exist_ok=True)
+
+    library_dependencies: dict[str, tuple[LibraryData, ...]] = {}
+
+    def get_library_dependencies(pypi_name: str) -> tuple[LibraryData, ...]:
+        if pypi_name not in library_dependencies:
+            root_lib = libraries[pypi_name]
+
+            # find all shared dependencies recursively
+            lib_names: set[str] = set()
+            lib_names_todo: set[str] = set(
+                root_lib.private_dependencies
+                + root_lib.public_dependencies
+                + root_lib.ext_dependencies
+            )
+
+            while lib_names_todo:
+                lib_name = lib_names_todo.pop()
+                if lib_name in lib_names:
+                    continue
+                lib_names.add(lib_name)
+                lib = libraries[lib_name]
+                lib_names_todo.update(
+                    lib.private_dependencies + lib.public_dependencies + lib.ext_dependencies
+                )
+
+            library_dependencies[pypi_name] = tuple(
+                libraries[pypi_name]
+                for pypi_name in sorted(lib_names, key=library_order.__getitem__)
+            )
+
+        return library_dependencies[pypi_name]
+
+    dependencies: tuple[LibraryData, ...] = get_library_dependencies(library_data.pypi_name)
+
+    shared_libs: tuple[LibraryData, ...] = tuple(
+        lib for lib in dependencies if lib.library_type == LibraryType.Shared
+    )
+
+    with open(
+        os.path.join(action_dir, "action.yml"), "w", encoding="utf-8"
+    ) as f:
+        f.write(
+            f"""name: 'Install Specialised {library_data.repo_name}'
+description: 'Build, publish and install this library specialised for the installed compiler. Requires Python, build and twine.'
+inputs:
+  twine-username:
+    description: 'The twine username'
+    required: true
+  twine-password:
+    description: 'The twine password'
+    required: true
+  compiler-specifier:
+    description: 'The PEP 440 version specifier for the compiler library'
+    required: true{
+"".join(f"""
+  {lib.short_var_name.replace("_", "-")}-specifier:
+    description: 'The PEP 440 version specifier for the {lib.repo_name} library'
+    required: true"""
+        for lib in dependencies
+        )
+}{
+f"""
+  {library_data.short_var_name.replace("_", "-")}-specifier:
+    description: 'The PEP 440 version specifier for the {library_data.repo_name} library'
+    required: true"""
+}
+outputs:
+  version:
+    description: "The version number of the installed library."
+    value: ${{{{ steps.get-version.outputs.version }}}}
+runs:
+  using: "composite"
+  steps:
+    - name: Validate Inputs
+      shell: bash
+      run: |
+        if [ -z "${{{{ inputs.twine-username }}}}" ]; then
+          echo "twine-username is empty"
+          exit 1
+        fi
+        
+        if [ -z "${{{{ inputs.twine-password }}}}" ]; then
+          echo "twine-password is empty"
+          exit 1
+        fi
+        
+        if [ -z "${{{{ inputs.compiler-specifier }}}}" ]; then
+          echo "compiler-specifier is empty"
+          exit 1
+        fi{
+"".join(f"""
+
+        if [ -z "${{{{ inputs.{lib.short_var_name.replace("_", "-")}-specifier }}}}" ]; then
+          echo "{lib.short_var_name.replace("_", "-")}-specifier is empty"
+          exit 1
+        fi""" for lib in dependencies)
+}
+
+        if [ -z "${{{{ inputs.{library_data.short_var_name.replace("_", "-")}-specifier }}}}" ]; then
+          echo "{library_data.short_var_name.replace("_", "-")}-specifier is empty"
+          exit 1
+        fi
+
+    - name: Install Prebuilt
+      id: install
+      shell: bash
+      continue-on-error: true
+      run: |
+        python -m pip install --only-binary {library_data.pypi_name} amulet-compiler-version${{{{ inputs.compiler-specifier }}}} {" ".join(f"{lib.pypi_name}${{{{ inputs.{lib.short_var_name.replace("_", "-")}-specifier }}}}" for lib in dependencies)} {library_data.pypi_name}${{{{ inputs.{library_data.short_var_name.replace("_", "-")}-specifier }}}}
+
+    - name: Build
+      if: steps.install.outcome == 'failure'
+      shell: bash
+      env:
+        AMULET_FREEZE_COMPILER: 1{
+"".join(
+    f"""
+        {lib.var_name.upper()}_REQUIREMENT: ${{{{ inputs.{lib.short_var_name.replace("_", "-")}-specifier }}}}"""
+    for lib in dependencies
+)
+        }
+      run: |
+        python -m build --wheel "${{{{ github.action_path }}}}"/../../..
+
+    - name: Publish
+      if: steps.install.outcome == 'failure'
+      shell: bash
+      env:
+        TWINE_USERNAME: ${{{{ inputs.twine-username }}}}
+        TWINE_PASSWORD: ${{{{ inputs.twine-password }}}}
+      run: |
+        twine upload "${{{{ github.action_path }}}}"/../../../dist/* --skip-existing
+
+    - name: Install
+      if: steps.install.outcome == 'failure'
+      shell: bash
+      run: |
+        python -m pip install "${{{{ github.action_path }}}}"/../../../dist/{library_data.pypi_name.replace("-", "_")}-*.whl
+
+    - name: Get __version__
+      id: get-version
+      shell: bash
+      run: |
+        version=$(python -c "import {library_data.import_name}; print({library_data.import_name}.__version__)")
+        echo "version=$version" >> "$GITHUB_OUTPUT"
+"""
+        )
