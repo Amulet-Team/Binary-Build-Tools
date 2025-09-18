fbe2b06a42bfd5cc362d310c6815f73e936a0892
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-09-18 10:52:03 +0100

    Bump mypy min python version

--- binary_build_tools/make_mypy_ini.py
+++ binary_build_tools/make_mypy_ini.py
@@ -10,7 +10,7 @@ def write(project_path: str, library_data: LibraryData) -> None:
 disallow_untyped_defs = True
 check_untyped_defs = True
 warn_return_any = True
-python_version = 3.11
+python_version = 3.12
 mypy_path = $MYPY_CONFIG_FILE_DIR/src
 packages = {library_data.root_import_name}
 """
