52a8c2f55dfa965d42a99bfd28193d5f9b75bf3d
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-08-07 09:00:38 +0100

    Add thread safety analysis to tests

--- binary_build_tools/make_tests/make_cmakelists.py
+++ binary_build_tools/make_tests/make_cmakelists.py
@@ -3,6 +3,24 @@ import os
 from binary_build_tools.data import LibraryData, libraries, library_order
 
 
+def get_module_properties(module_name: str, library_data: LibraryData, indent: str = "") -> str:
+    return f"""\
+set_target_properties({module_name} PROPERTIES CXX_VISIBILITY_PRESET hidden)
+{indent}set_target_properties({module_name} PROPERTIES FOLDER "Tests")
+{indent}target_compile_definitions({module_name} PRIVATE PYBIND11_DETAILED_ERROR_MESSAGES)
+{indent}target_compile_definitions({module_name} PRIVATE PYBIND11_VERSION="${{pybind11_VERSION}}")
+{indent}target_compile_definitions({module_name} PRIVATE COMPILER_ID="${{CMAKE_CXX_COMPILER_ID}}")
+{indent}target_compile_definitions({module_name} PRIVATE COMPILER_VERSION="${{CMAKE_CXX_COMPILER_VERSION}}"){
+"".join(
+    f"""
+{indent}target_link_libraries({module_name} PRIVATE {libraries[lib_name].cmake_lib_name})"""
+    for lib_name in sorted(library_data.test_dependencies + (library_data.pypi_name,), key=library_order.__getitem__)
+    if lib_name != "pybind11" and libraries[lib_name].cmake_lib_name is not None
+)
+}
+{indent}target_sources({module_name} PRIVATE ${{SOURCES}})
+"""
+
 def write(tests_path: str, library_data: LibraryData) -> None:
     if library_data.lib_name is None:
         return
@@ -10,6 +28,8 @@ def write(tests_path: str, library_data: LibraryData) -> None:
         f.write(f"""cmake_minimum_required(VERSION 4.1)
 
 project({library_data.cmake_package}_tests LANGUAGES CXX)
+
+option(ENABLE_CLANG_THREAD_SAFETY_ANALYSIS "Run clang -Wthread-safety over the sources before building" OFF)
 
 # Set C++20
 set(CMAKE_CXX_STANDARD 20)
@@ -45,26 +65,21 @@ endif()"""
 file(GLOB_RECURSE SOURCES LIST_DIRECTORIES false "${{CMAKE_CURRENT_LIST_DIR}}/*.py.cpp")
 
 pybind11_add_module(_test_{library_data.cmake_package})
-set_target_properties(_test_{library_data.cmake_package} PROPERTIES CXX_VISIBILITY_PRESET hidden)
-set_target_properties(_test_{library_data.cmake_package} PROPERTIES FOLDER "Tests")
-target_compile_definitions(_test_{library_data.cmake_package} PRIVATE PYBIND11_DETAILED_ERROR_MESSAGES)
-target_compile_definitions(_test_{library_data.cmake_package} PRIVATE PYBIND11_VERSION="${{pybind11_VERSION}}")
-target_compile_definitions(_test_{library_data.cmake_package} PRIVATE COMPILER_ID="${{CMAKE_CXX_COMPILER_ID}}")
-target_compile_definitions(_test_{library_data.cmake_package} PRIVATE COMPILER_VERSION="${{CMAKE_CXX_COMPILER_VERSION}}"){
-"".join(
-    f"""
-target_link_libraries(_test_{library_data.cmake_package} PRIVATE {libraries[lib_name].cmake_lib_name})"""
-    for lib_name in sorted(library_data.test_dependencies + (library_data.pypi_name,), key=library_order.__getitem__)
-    if lib_name != "pybind11" and libraries[lib_name].cmake_lib_name is not None
-)
-}
-target_sources(_test_{library_data.cmake_package} PRIVATE ${{SOURCES}})
+{get_module_properties(f"_test_{library_data.cmake_package}", library_data)}\
 foreach(FILE ${{SOURCES}})
     file(RELATIVE_PATH REL_PATH ${{CMAKE_CURRENT_LIST_DIR}} ${{FILE}})
     get_filename_component(GROUP ${{REL_PATH}} DIRECTORY)
     string(REPLACE "/" "\\\\" GROUP "${{GROUP}}")
     source_group(${{GROUP}} FILES ${{FILE}})
 endforeach()
+
+if(THREAD_SAFETY_ANALYSIS)
+    add_library(_test_{library_data.cmake_package}_thread_analysis OBJECT)
+    target_link_libraries({library_data.cmake_package}_thread_analysis PRIVATE pybind11::module)
+    {get_module_properties(f"{library_data.cmake_package}_thread_analysis", library_data, indent="\t")}\
+    target_compile_options({library_data.cmake_package}_thread_analysis PRIVATE -fsyntax-only -Wthread-safety -Werror=thread-safety)
+    add_dependencies(_test_{library_data.cmake_package} _test_{library_data.cmake_package}_thread_analysis)
+endif()
 
 # Install
 install(TARGETS _test_{library_data.cmake_package} DESTINATION "${{CMAKE_CURRENT_LIST_DIR}}/test_{library_data.cmake_package}")
